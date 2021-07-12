70004e01dbeee9814cdef810d258ff6869a64704
Jithma Gamage <jithmagamage99@gmail.com>
2021-07-12 15:21:26 +0530

    Add Test Code

---
+++ venv/api.py
@@ -0,0 +1,36 @@
+from enum import unique
+from flask import Flask
+app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db=SQLAlchemy(app)
+
+
+class Drink(db.Modle):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.Strring(80),unique=True,Nullable=False)
+    description=db.Column(db.String(120))
+
+    def __reper__(self):
+        
+        return f"{self.name}-{self.description}"
+
+
+    
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello'
+    
+
+@app.route('/drinks')
+def get_drinks():
+   return {"Drinks":"Kasiya"}
+
+app.run(host='localhost', port=3002)
+
+ 
+    
+
