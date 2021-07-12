a15ba1e92c517964e74daa6bbd70fc24622aaf79
Jithma Gamage <jithmagamage99@gmail.com>
2021-07-12 15:48:36 +0530

    Change the db.Model

--- venv/api.py
+++ venv/api.py
@@ -7,9 +7,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db=SQLAlchemy(app)
 
 
-class Drink(db.Modle):
+class Drink(db.Model):
     id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.Strring(80),unique=True,Nullable=False)
+    name=db.Column(db.String(80),unique=True,nullable=False)
     description=db.Column(db.String(120))
 
     def __reper__(self):
